cc1cd81d5a79d0102592bec8b25f9942fbe82742
sw08 <yswysw0518@gmail.com>
2023-08-15 18:54:50 +0900

    Docs: Remove wrong files

--- js/bg.js
+++
@@ -1,4 +0,0 @@
-document.addEventListener('scroll', () => {
-    const homeheight = document.getElementById('intro').getBoundingClientRect().height + window.innerHeight * 0.25
-    document.getElementById('bg').style.filter = `blur(${(window.scrollY <= homeheight ? window.scrollY / homeheight : 1) * 5}px)`
-})
