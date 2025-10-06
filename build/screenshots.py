f88ea6ee3f009673ec5c20bd8d3be758c6fb295a
Seungwoo Yi <yswysw0518@gmail.com>
2025-10-06 13:27:04 +0900

    New: Cache screenshots and pip modules during build

--- build/screenshots.py
+++ build/screenshots.py
@@ -1,15 +1,20 @@
 import os
+import shutil
 from PIL import Image
 
-os.chdir("./source/screenshots")
-os.mkdir("1080p")  # desktop high
-os.mkdir("720p")  # mobile high / desktop middle
-os.mkdir("360p")  # mobile middle / desktop low
-os.mkdir("144p")  # mobile small
+os.chdir("./screenshots")
+if not os.path.isdir("1080p"):
+    os.mkdir("1080p")  # desktop high
+if not os.path.isdir("720p"):
+    os.mkdir("720p")  # mobile high / desktop middle
+if not os.path.isdir("360p"):
+    os.mkdir("360p")  # mobile middle / desktop low
+if not os.path.isdir("144p"):
+    os.mkdir("144p")  # mobile small
 for i in os.listdir():
-    if os.path.isfile(i):
+    if (not os.path.isfile(f"1080p/{i}")) and os.path.isfile(i):
         img = Image.open(i)
         img.resize((1280, 720)).save(f"720p/{i}")
         img.resize((640, 360)).save(f"360p/{i}")
         img.resize((256, 144)).save(f"144p/{i}")
-        os.rename(i, f"1080p/{i}")
+        shutil.copy(i, f"1080p/{i}")
