bce23ea00877f258fab061407e6525cca305455e
Seungwoo Yi <yswysw0518@gmail.com>
2026-01-11 21:43:16 +0900

    New: Add Gallery Page

--- build/database.py
+++ build/database.py
@@ -5,12 +5,10 @@ os.chdir("./screenshots")
 
 files = [i for i in os.listdir() if os.path.isfile(i)]
 
-by_date, by_acft, by_lvry, by_arpt, tags = (
-    defaultdict(lambda: defaultdict(lambda: defaultdict(list))),
+by_acft, by_lvry, by_arpt = (
     defaultdict(list),
     defaultdict(list),
-    defaultdict(list),
-    defaultdict(list),
+    defaultdict(list)
 )
 
 for i in files:
@@ -21,25 +19,12 @@ for i in files:
     else:
         by_lvry[i[0]].append(filename)
         by_acft[i[1]].append(filename)
-    date = i[-2]
-    year = date[:4]
-    month = date[4:6]
-    day = date[6:8]
-    by_date[year][month][day].append(filename)
 
 if os.path.isdir("../database"):
     shutil.rmtree("../database")
 os.mkdir("../database")
 os.chdir("../database")
 
-tags = {
-    "by_acft": list(by_acft.keys()),
-    "by_lvry": list(by_lvry.keys()),
-    "by_arpt": list(by_arpt.keys()),
-}
-
-with open("by_date.json", "w") as f:
-    json.dump(by_date, f)
 
 with open("by_acft.json", "w") as f:
     json.dump(by_acft, f)
@@ -48,5 +33,5 @@ with open("by_lvry.json", "w") as f:
 with open("by_arpt.json", "w") as f:
     json.dump(by_arpt, f)
 
-with open("tags.json", "w") as f:
-    json.dump(tags, f)
+with open("all_files.json", "w") as f:
+    json.dump(files, f)
--- build/screenshots.py
+++ build/screenshots.py
@@ -4,17 +4,11 @@ from PIL import Image
 
 os.chdir("./screenshots")
 if not os.path.isdir("1080p"):
-    os.mkdir("1080p")  # desktop high
-if not os.path.isdir("720p"):
-    os.mkdir("720p")  # mobile high / desktop middle
+    os.mkdir("1080p")  # high
 if not os.path.isdir("360p"):
-    os.mkdir("360p")  # mobile middle / desktop low
-if not os.path.isdir("144p"):
-    os.mkdir("144p")  # mobile small
+    os.mkdir("360p")  # low
 for i in os.listdir():
     if (not os.path.isfile(f"1080p/{i}")) and os.path.isfile(i):
         img = Image.open(i)
-        img.resize((1280, 720)).save(f"720p/{i}")
         img.resize((640, 360)).save(f"360p/{i}")
-        img.resize((256, 144)).save(f"144p/{i}")
         shutil.copy(i, f"1080p/{i}")
